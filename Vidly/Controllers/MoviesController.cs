652b285a250a592445f18a23317b624afa65aa97
kavaguchi <hugo.kavaguchi@gmail.com>
2017-11-20 16:33:35 -0200

    More

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return View("year: " + year + " month: " + month);
+        }
+
+    }
+}
